0009c854976ac83f0b2704cacced5602f32ed90f
Ahmadmansoor <safi43@yahoo.com>
2017-12-16 17:40:43 +0400

    HookKeyBord
    get drop file path
    get drop shortcut path
    get file icon in PictureBox

---
+++ AdvancedToolBar/Form1.vb
@@ -0,0 +1,74 @@
+﻿Imports Shell32
+Imports System.Runtime.InteropServices
+Public Class Form1
+
+
+    'Private WithEvents kbHook As New KeyboardHook
+
+    'Private Sub kbHook_KeyDown(ByVal Key As System.Windows.Forms.Keys) Handles kbHook.KeyDown
+    '    Dim mousepos As Rectangle = Screen.PrimaryScreen.WorkingArea
+    '    Dim x = MousePosition.X - Screen.PrimaryScreen.Bounds.X
+    '    Dim y = MousePosition.Y
+    '    If Key = 162 And x >= mousepos.Width - 10 Then
+    '        Me.Show()
+    '    End If
+    'End Sub
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Me.Hide()
+        Timer1.Stop()
+        Timer1.Enabled = False
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        'e.Cancel = True
+        'Me.Hide()
+    End Sub
+
+    Private Sub Form1_DragEnter(sender As Object, e As DragEventArgs) Handles MyBase.DragEnter
+        If e.Data.GetDataPresent(DataFormats.FileDrop) Then
+            e.Effect = DragDropEffects.Copy
+        End If
+    End Sub
+
+    Private Sub Form1_DragDrop(sender As Object, e As DragEventArgs) Handles MyBase.DragDrop
+        Dim fileName() As String = e.Data.GetData(DataFormats.FileDrop)
+        'MsgBox(GetLnkTarget(fileName(0)))
+        'Dim myIcon As System.Drawing.Icon = Image.FromFile(fileName(0))
+        'PictureBox1.Image = Image.FromFile(fileName(0))
+
+
+        Dim hImgLarge As IntPtr  'The handle to the system image list.
+        Dim shinfo As SHFILEINFO
+        shinfo = New SHFILEINFO()
+        Dim openFileDialog1 As OpenFileDialog
+        openFileDialog1 = New OpenFileDialog()
+
+        shinfo.szDisplayName = New String(Chr(0), 260)
+        shinfo.szTypeName = New String(Chr(0), 80)
+
+        'Use this to get the small icon.
+        'hImgSmall = SHGetFileInfo(fName, 0, shinfo, Marshal.SizeOf(shinfo), SHGFI_ICON Or SHGFI_SMALLICON)
+
+        'Use this to get the large icon.
+        hImgLarge = SHGetFileInfo(fileName(0), 0, shinfo, Marshal.SizeOf(shinfo), SHGFI_ICON Or SHGFI_LARGEICON)
+
+        'The icon is returned in the hIcon member of the shinfo struct.
+        Dim myIcon As System.Drawing.Icon = System.Drawing.Icon.FromHandle(shinfo.hIcon)
+        PictureBox1.Image = myIcon.ToBitmap
+
+    End Sub
+
+    Private Sub Form1_MouseClick(sender As Object, e As MouseEventArgs) Handles MyBase.MouseClick
+        If (e.Button = MouseButtons.Right) Then
+            CMS.Parent = Me.Parent
+            CMS.Show()
+            CMS.Location = New Point(e.X + Me.Left, e.Y + Me.Top)
+
+            CMS.Items.Add("test")
+        End If
+    End Sub
+End Class
---
+++ AdvancedToolBar/PublicFunction.vb
@@ -0,0 +1,27 @@
+﻿Imports System.Runtime.InteropServices
+Module PublicFunction
+    Public Structure SHFILEINFO
+        Public hIcon As IntPtr            ' : icon
+        Public iIcon As Integer           ' : icondex
+        Public dwAttributes As Integer    ' : SFGAO_ flags
+        <MarshalAs(UnmanagedType.ByValTStr, SizeConst:=260)>
+        Public szDisplayName As String
+        <MarshalAs(UnmanagedType.ByValTStr, SizeConst:=80)>
+        Public szTypeName As String
+    End Structure
+
+    Public Declare Auto Function SHGetFileInfo Lib "shell32.dll" (ByVal pszPath As String, ByVal dwFileAttributes As Integer, ByRef psfi As SHFILEINFO, ByVal cbFileInfo As Integer, ByVal uFlags As Integer) As IntPtr
+
+    Public Const SHGFI_ICON = &H100
+    Public Const SHGFI_SMALLICON = &H1
+    Public Const SHGFI_LARGEICON = &H0
+    Public Function GetLnkTarget(lnkPath As String) As String
+        Dim shl = New Shell32.Shell()
+        ' Move this to class scope
+        lnkPath = System.IO.Path.GetFullPath(lnkPath)
+        Dim dir = shl.[NameSpace](System.IO.Path.GetDirectoryName(lnkPath))
+        Dim itm = dir.Items().Item(System.IO.Path.GetFileName(lnkPath))
+        Dim lnk = DirectCast(itm.GetLink, Shell32.ShellLinkObject)
+        Return lnk.Target.Path
+    End Function
+End Module
